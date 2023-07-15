61798b893389b70db4ee73955ca2523ee01b1506
Homer <45139235+buckhorn17@users.noreply.github.com>
2023-07-15 14:04:34 +0800

    version1.0

---
+++ all.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  
+    $('.hamBar-menu').click(function(){
+      $(".header-menu").fadeToggle(200);
+      $(".header-menu").css("display","flex");
+      })
+      
+});
